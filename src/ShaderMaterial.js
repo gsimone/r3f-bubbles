d376631671a61f0a1e21e3a0752873c9d6ed6701
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-08-01 01:00:45 +0200

    minor changes

--- src/ShaderMaterial.js
+++ src/ShaderMaterial.js
@@ -1,11 +1,13 @@
 import * as THREE from 'three'
 import React, { useRef, useEffect, useMemo } from 'react'
-import mergeRefs from 'merge-refs'
+
 import { useLoader, useFrame, useThree } from 'react-three-fiber'
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader'
 import { useControl } from 'react-three-gui'
+import { useTextureLoader } from 'drei'
+
+import mergeRefs from 'merge-refs'
+
 import './materials/DistortMaterial'
-import { TextureLoader } from 'three'
 
 const MATERIAL_GROUP = 'Material'
 const SHADER_GROUP = 'Shader'
@@ -38,22 +40,17 @@ const ShaderMaterial = React.forwardRef(function ShaderMaterial(props, forwarded
   const distort = useControl('distort', { group: SHADER_GROUP, type: 'number', value: 0.4, max: 1 })
 
   const bumpMap = useLoader(THREE.TextureLoader, './bump.jpg')
-  /*const [envMap] = useLoader(
-    THREE.CubeTextureLoader,
-    [['1.jpg', '2.jpg', '6.jpg', '4.jpg', '5.jpg', '3.jpg']],
-    (loader) => loader.setPath('cube/'),
-  )*/
 
   const { gl } = useThree()
-  const data = useLoader(TextureLoader, '/_DHQ.jpg')
+  const envMapTexture = useTextureLoader('/_DHQ.jpg')
   const envMap = useMemo(() => {
-    const gen = new THREE.PMREMGenerator(gl)
-    gen.compileEquirectangularShader()
-    const hdrCubeRenderTarget = gen.fromCubemap(data)
-    data.dispose()
-    gen.dispose()
+    const generator = new THREE.PMREMGenerator(gl)
+    generator.compileEquirectangularShader()
+    const hdrCubeRenderTarget = generator.fromCubemap(envMapTexture)
+    envMapTexture.dispose()
+    generator.dispose()
     return hdrCubeRenderTarget.texture
-  }, [data, gl])
+  }, [envMapTexture, gl])
 
   useEffect(() => {
     matRef.current.envMap = envMap
