f6e22f623f72b2667d65d6ee9cde5775a0416822
Paul Henschel <drcmda@gmail.com>
2020-08-01 14:20:57 +0200

    fix pp

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,26 @@
-const { addWebpackModuleRule, override } = require('customize-cra')
+const {
+  babelInclude,
+  addWebpackAlias,
+  removeModuleScopePlugin,
+  addWebpackModuleRule,
+  override,
+} = require('customize-cra')
 const { addReactRefresh } = require('customize-cra-react-refresh')
+const path = require('path')
 
 module.exports = (config, env) => {
+  config.resolve.extensions = [...config.resolve.extensions, '.ts', '.tsx']
   return override(
     addReactRefresh(),
+    /*removeModuleScopePlugin(),
+    babelInclude([path.resolve('src'), path.resolve('../react-postprocessing/src')]),
+    addWebpackAlias({
+      postprocessing: path.resolve('node_modules/postprocessing'),
+      react: path.resolve('node_modules/react'),
+      'react-dom': path.resolve('node_modules/react-dom'),
+      'react-three-fiber': path.resolve('node_modules/react-three-fiber'),
+      'react-postprocessing': path.resolve('../react-postprocessing/src/index.tsx'),
+    }),*/
     addWebpackModuleRule({ test: /\.(glsl|vs|fs|vert|frag)$/, use: ['raw-loader', 'glslify-loader'] })
   )(config, env)
 }
--- src/Effects.js
+++ src/Effects.js
@@ -5,7 +5,7 @@ import { KernelSize } from 'postprocessing'
 
 export default function Effects() {
   return (
-    <EffectComposer smma>
+    <EffectComposer>
       <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} />
       <Bloom kernelSize={KernelSize.VERY_LARGE} luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
       <Noise opacity={0.02} />
