bcd0228f61fb882f44dbcccb58ef820d2eaf279c
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-08-18 12:30:47 +0200

    upgrades deps

--- src/App.js
+++ src/App.js
@@ -93,9 +93,6 @@ export default function App() {
         <Scene />
         <EffectComposer>
           <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} />
-          <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} opacity={3} />
-          <Noise opacity={0.025} />
-          <Vignette eskil={false} offset={0.1} darkness={1.1} />
         </EffectComposer>
       </Suspense>
     </Canvas>
